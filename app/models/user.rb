1e39043fde79092515b158df0b70b1884bbdaba5
leticiaoliveira5 <leoliveira994@gmail.com>
2022-10-10 15:47:42 -0300

    adiciona validação em user

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  validate_presence_of :name
+
   # adiciona tipo de usuário
-  enum role: { admin: 0, member: 1 }
+  enum role: { admin: 0, member: 1 }, _suffix: true
 end
