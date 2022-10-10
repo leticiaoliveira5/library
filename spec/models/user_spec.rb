25fbb4c960e520fb42bafe2d445ce2b29bd8f8ad
leticiaoliveira5 <leoliveira994@gmail.com>
2022-10-10 15:38:12 -0300

    fix rubocop warning

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,11 +2,11 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   context 'validations' do
-    it { should validate_presence_of(:email) }
-    it { should validate_presence_of(:name) }
-    it { should validate_presence_of(:password) }
-    it { should validate_uniqueness_of(:email).ignoring_case_sensitivity }
-    it { should allow_values(:member, :admin).for(:role) }
+    it { is_expected.to validate_presence_of(:email) }
+    it { is_expected.to validate_presence_of(:name) }
+    it { is_expected.to validate_presence_of(:password) }
+    it { is_expected.to validate_uniqueness_of(:email).ignoring_case_sensitivity }
+    it { is_expected.to allow_values(:member, :admin).for(:role) }
 
     it 'has a default user role' do
       expect(subject).to be_user_role
