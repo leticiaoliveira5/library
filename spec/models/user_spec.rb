b853e43c54c227acf65dc5d77773205cbdc1f041
leticiaoliveira5 <leoliveira994@gmail.com>
2022-10-10 15:53:35 -0300

    remove teste com subject

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -7,9 +7,5 @@ RSpec.describe User, type: :model do
     it { is_expected.to validate_presence_of(:password) }
     it { is_expected.to validate_uniqueness_of(:email).ignoring_case_sensitivity }
     it { is_expected.to allow_values(:member, :admin).for(:role) }
-
-    it 'has a default user role' do
-      expect(subject).to be_user_role
-    end
   end
 end
