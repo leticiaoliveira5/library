b5415625f07a1e9b50d45aa59dbf0360db9b3a78
leticiaoliveira5 <leoliveira994@gmail.com>
2022-10-12 23:13:37 -0300

    add campos na migration

--- db/migrate/20221013015421_create_reservations.rb
+++ db/migrate/20221013015421_create_reservations.rb
@@ -3,6 +3,10 @@ class CreateReservations < ActiveRecord::Migration[7.0]
     create_table :reservations do |t|
 
       t.timestamps
+      t.integer :user_id, null: false
+      t.integer :book_id, null: false
+      t.integer :status
+      t.datetime :devolution
     end
   end
 end
