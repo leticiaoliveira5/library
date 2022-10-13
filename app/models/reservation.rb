757abdfadf04a3e26e2a81c6b7c72a303a457e49
leticiaoliveira5 <leoliveira994@gmail.com>
2022-10-13 00:26:36 -0300

    adiciona scopes em reservation

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -2,6 +2,11 @@ class Reservation < ApplicationRecord
   belongs_to :book
   belongs_to :user
 
+  # Reservas com devolução atrasada
+  scope :overdue, -> { where(status: 'active', devolution: ...DateTime.now) }
+  scope :active, -> { where(status: 'active') }
+  scope :finished, -> { where(status: 'finished') }
+
   # A reserva pode estar ativa, atrasada, ou finalizada
   enum status: { active: 0, finished: 1 }
 end
