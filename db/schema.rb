dffbdff7263433e5b7a5e0d23831bc64d0873465
leticiaoliveira5 <leoliveira994@gmail.com>
2022-10-12 23:14:15 -0300

    update schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_13_013600) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_13_015421) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -33,6 +33,15 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_13_013600) do
     t.datetime "updated_at", null: false
     t.string "name", null: false
     t.index ["name"], name: "index_categories_on_name", unique: true
+  end
+
+  create_table "reservations", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "user_id", null: false
+    t.integer "book_id", null: false
+    t.integer "status"
+    t.datetime "devolution"
   end
 
   create_table "users", force: :cascade do |t|
