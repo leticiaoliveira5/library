57fdbab880fdfa739318fe148df8feeffc5f999d
leticiaoliveira5 <leoliveira994@gmail.com>
2022-10-12 23:20:02 -0300

    adiciona enum status para reservation

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,4 +1,8 @@
 class Reservation < ApplicationRecord
   belongs_to :book
   belongs_to :user
+
+  # a reserva pode estar ativa, atrasada, ou finalizada
+  enum status: { active: 0, overdue: 1, finished: 2 }
+
 end
