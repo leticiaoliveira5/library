b86a029554899941782867dbbc7e0df21cbd8e3e
leticiaoliveira5 <leoliveira994@gmail.com>
2022-10-13 09:37:11 -0300

    adiciona testes de model relations

--- spec/models/author_spec.rb
+++ spec/models/author_spec.rb
@@ -3,5 +3,6 @@ require 'rails_helper'
 RSpec.describe Author, type: :model do
   context 'validations' do
     it { is_expected.to validate_presence_of(:name) }
+    it { is_expected.to have_many(:books) }
   end
 end
