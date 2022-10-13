184c03fc3d04770fd5dd0cbc7cc020c8da7180d4
leticiaoliveira5 <leoliveira994@gmail.com>
2022-10-13 10:49:01 -0300

    adiciona actions show e create

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -3,7 +3,13 @@ class BooksController < ApplicationController
 
   def index; end
 
+  def show
+    @book = Book.find(params[:id])
+  end
+
   def new; end
+
+  def create; end
 
   def edit; end
 end
