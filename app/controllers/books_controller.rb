1f117d5c010e56921638a2e6cfdae3692f936b28
leticiaoliveira5 <leoliveira994@gmail.com>
2022-10-13 10:53:00 -0300

    adiciona queries ao controller

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,15 +1,29 @@
 class BooksController < ApplicationController
   before_action :authenticate_user!
 
-  def index; end
+  def index
+    @books = Book.all
+  end
 
   def show
     @book = Book.find(params[:id])
   end
 
-  def new; end
+  def new
+    @book = Book.new
+    @authors = Author.all
+    @categories = Category.all
+  end
 
-  def create; end
+  def create
+    @book = Book.new(book_params)
+  end
 
-  def edit; end
+  def edit
+    @book = Book.find(params[:id])
+  end
+
+  def book_params
+    params.require(:book).permit(:title, :author_id, :category_id)
+  end
 end
