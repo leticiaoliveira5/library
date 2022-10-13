0fef61b4c4aafa5a3780a36d894bf9315876cc05
leticiaoliveira5 <leoliveira994@gmail.com>
2022-10-12 22:37:18 -0300

    recria migration

--- db/migrate/20221013013600_create_books.rb
+++ db/migrate/20221013013600_create_books.rb
@@ -5,7 +5,7 @@ class CreateBooks < ActiveRecord::Migration[7.0]
       t.timestamps
       t.string :title, null: false
       t.integer :author_id, null: false
-      t.integer :genre_id, null: false
+      t.integer :category_id, null: false
     end
   end
 end
