bb87b55d9491ab25097a0c06145c1e502324f963
leticiaoliveira5 <leoliveira994@gmail.com>
2022-10-12 22:20:49 -0300

    corrige migration

--- db/migrate/20221013011618_create_categories.rb
+++ db/migrate/20221013011618_create_categories.rb
@@ -1,9 +1,8 @@
 class CreateCategories < ActiveRecord::Migration[7.0]
   def change
     create_table :categories do |t|
-
       t.timestamps
-      t.string :name, null: false, index: { unique: true }
+      t.string :name, null: false, unique: true
     end
   end
 end
