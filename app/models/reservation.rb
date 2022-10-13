9897b177305aa0fc9f99ed7f094ad3b0e760c81e
leticiaoliveira5 <leoliveira994@gmail.com>
2022-10-13 12:44:15 -0300

    add validação para reserva

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -2,6 +2,11 @@ class Reservation < ApplicationRecord
   belongs_to :book
   belongs_to :user
 
+  validates :devolution, presence: true
+  # A data de devolução não deve ser maior que duas semanas
+  # a partir da data de criação da reserva
+  validate :not_longer_than_two_weeks, unless: -> { devolution.blank? }, on: :create
+
   # Reservas com devolução atrasada
   scope :overdue, -> { where(status: 'active', devolution: ...DateTime.now) }
   scope :active, -> { where(status: 'active') }
@@ -9,4 +14,10 @@ class Reservation < ApplicationRecord
 
   # A reserva pode estar ativa, atrasada, ou finalizada
   enum status: { active: 0, finished: 1 }
+
+  def not_longer_than_two_weeks
+    return unless devolution > DateTime.now + 2.weeks
+
+    errors.add(:devolution, :longer_than_two_weeks)
+  end
 end
