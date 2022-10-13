5a895e37aed4e120cd0e9a3d562cb1834c058906
leticiaoliveira5 <leoliveira994@gmail.com>
2022-10-12 23:22:28 -0300

    adiciona testes para reservation

--- spec/models/reservation_spec.rb
+++ spec/models/reservation_spec.rb
@@ -1,5 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe Reservation, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context 'validations' do
+    it { is_expected.to belong_to(:user) }
+    it { is_expected.to belong_to(:book) }
+    it { is_expected.to allow_values(:active, :finished, :overdue).for(:status) }
+  end
 end
