f2533e6e18004064c30994ca0f084e8e7ba54a00
leticiaoliveira5 <leoliveira994@gmail.com>
2022-10-13 16:19:39 -0300

    resolve rubocop warnings

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -2,6 +2,7 @@ require 'rails_helper'
 
 RSpec.describe 'Users', type: :request do
   let(:admin) { create(:user, :admin) }
+
   before { sign_in(admin) }
 
   describe 'GET /new' do
@@ -24,8 +25,8 @@ RSpec.describe 'Users', type: :request do
 
   describe 'POST /create' do
     it 'creates a new user' do
-      post users_path, params: { user: { email: 'anamuniz@bol.net', password: '123456a', 
-                                        name: 'Ana Muniz', role: :member } }
+      post users_path, params: { user: { email: 'anamuniz@bol.net', password: '123456a',
+                                         name: 'Ana Muniz', role: :member } }
 
       expect(response).to redirect_to(User.last)
       expect(flash[:notice]).to be_present
