75bc82d9bf72fd9757e855b4e548ee39bf583d9d
leticiaoliveira5 <leoliveira994@gmail.com>
2022-10-13 16:17:16 -0300

    implementa testes para actions

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,7 +1,34 @@
 require 'rails_helper'
 
-RSpec.describe "Users", type: :request do
-  describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+RSpec.describe 'Users', type: :request do
+  let(:admin) { create(:user, :admin) }
+  before { sign_in(admin) }
+
+  describe 'GET /new' do
+    it 'show user form' do
+      get new_user_path
+
+      expect(response).to be_successful
+    end
+  end
+
+  describe 'GET /show' do
+    it 'show user by id' do
+      user = create(:user, :member)
+
+      get user_path(user)
+
+      expect(response).to be_successful
+    end
+  end
+
+  describe 'POST /create' do
+    it 'creates a new user' do
+      post users_path, params: { user: { email: 'anamuniz@bol.net', password: '123456a', 
+                                        name: 'Ana Muniz', role: :member } }
+
+      expect(response).to redirect_to(User.last)
+      expect(flash[:notice]).to be_present
+    end
   end
 end
