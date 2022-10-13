007d22b70e0f013301f9ef44be15189fb5cb4bf5
leticiaoliveira5 <leoliveira994@gmail.com>
2022-10-12 23:41:43 -0300

    remove warning do rubocop

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -2,7 +2,6 @@ class Reservation < ApplicationRecord
   belongs_to :book
   belongs_to :user
 
-  # a reserva pode estar ativa, atrasada, ou finalizada
+  # A reserva pode estar ativa, atrasada, ou finalizada
   enum status: { active: 0, overdue: 1, finished: 2 }
-
 end
