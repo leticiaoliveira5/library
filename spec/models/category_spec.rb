46784418763e0f34b6352b1240c5b8a2fa6e8442
leticiaoliveira5 <leoliveira994@gmail.com>
2022-10-12 22:33:26 -0300

    resolve rubocop

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe Category, type: :model do
   subject { build(:category) }
-  
+
   context 'validations' do
     it { is_expected.to validate_presence_of(:name) }
     it { is_expected.to validate_uniqueness_of(:name) }
