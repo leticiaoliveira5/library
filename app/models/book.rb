7167a5e09384dfbac7d98511b3dc6fa3de1d95a3
leticiaoliveira5 <leoliveira994@gmail.com>
2022-10-12 23:11:55 -0300

    adiciona opção dependent a book

--- app/models/book.rb
+++ app/models/book.rb
@@ -3,7 +3,7 @@ class Book < ApplicationRecord
   # e possui reservas, mas apenas uma deve estar ativa
   belongs_to :author
   belongs_to :category
-  has_many :reservations
+  has_many :reservations, dependent: nullify
 
   validates :title, presence: true
 end
