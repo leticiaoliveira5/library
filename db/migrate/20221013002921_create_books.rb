ee4bf11ccfaea97a709abcd95b6caf41845e1558
leticiaoliveira5 <leoliveira994@gmail.com>
2022-10-12 21:43:58 -0300

    adiciona campos na migration

--- db/migrate/20221013002921_create_books.rb
+++ db/migrate/20221013002921_create_books.rb
@@ -3,6 +3,9 @@ class CreateBooks < ActiveRecord::Migration[7.0]
     create_table :books do |t|
 
       t.timestamps
+      t.string :title, null: false
+      t.integer :author_id, null: false
+      t.integer :genre_id, null: false
     end
   end
 end
