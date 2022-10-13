cd4496759bc3b687046373a554e9cfe1eaa910da
leticiaoliveira5 <leoliveira994@gmail.com>
2022-10-12 22:11:35 -0300

    adiciona campo na migration

--- db/migrate/20221013010601_create_categories.rb
+++ db/migrate/20221013010601_create_categories.rb
@@ -3,6 +3,7 @@ class CreateCategories < ActiveRecord::Migration[7.0]
     create_table :categories do |t|
 
       t.timestamps
+      t.string :name, null: false, index: { unique: true }
     end
   end
 end
