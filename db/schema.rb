ec8259e610e45913050f7413044526009e479514
leticiaoliveira5 <leoliveira994@gmail.com>
2022-10-12 21:54:13 -0300

    roda a migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_10_150342) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_13_004941) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "authors", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "name", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
