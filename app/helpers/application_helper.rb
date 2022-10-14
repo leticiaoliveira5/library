198dd41a5698d3a81be970beb73eb35ff7fde1e1
leticiaoliveira5 <leoliveira994@gmail.com>
2022-10-14 10:36:41 -0300

    usa links pra prevenir o post

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,12 +5,12 @@ module ApplicationHelper
   end
 
   def book_admin_links(book)
-    button_to 'Editar', edit_book_path(book), class: 'btn btn-primary'
+    link_to 'Editar', edit_book_path(book), class: 'btn btn-primary'
   end
 
   def book_member_links(book)
     if book.reservations.active.empty?
-      button_to 'Reservar', new_reservation_path, class: 'btn btn-primary'
+      link_to 'Reservar', new_reservation_path, class: 'btn btn-primary'
     else
       content_tag(:div, 'O livro não está disponível no momento', class: 'danger')
     end
