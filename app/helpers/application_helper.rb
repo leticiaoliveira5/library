6e46d72b5b3cccdcbfa276c0c17c78488d212fe7
leticiaoliveira5 <leoliveira994@gmail.com>
2022-10-14 10:27:16 -0300

    melhora view com helpers

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,18 @@
 module ApplicationHelper
+  def book_cover(book)
+    cover = book.cover.attached? ? book.cover : 'no-cover.jpg'
+    image_tag(cover, width: '100%')
+  end
+
+  def book_admin_links(book)
+    button_to 'Editar', edit_book_path, class: 'btn btn-primary'
+  end
+
+  def book_member_links(book)
+    if book.reservations.active.empty?
+      button_to 'Reservar', new_reservation_path, class: 'btn btn-primary'
+    else
+      content_tag(:div, 'O livro não está disponível no momento')
+    end
+  end
 end
