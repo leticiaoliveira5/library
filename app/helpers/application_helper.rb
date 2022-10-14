6f67b180d79bbac65230109c91bc1346d0b034d4
leticiaoliveira5 <leoliveira994@gmail.com>
2022-10-14 15:33:24 -0300

    melhora métodos helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,19 +1,23 @@
 module ApplicationHelper
   def book_cover(book)
     cover = book.cover.attached? ? book.cover : 'no-cover.jpg'
-    link_to image_tag(cover, width: '100%'), book
+    image_tag(cover, width: '100%')
   end
 
   def book_admin_links(book)
-    if book.active_reservations.blank?
-      link_to 'Criar nova reserva', new_reservation_path, class: 'btn btn-primary'
-    end
-    .concat(link_to 'Editar', edit_book_path(book), class: 'btn btn-primary', style: 'margin-left: 5px;')
+    link_to('Editar', edit_book_path(book), class: 'btn btn-primary', style: 'margin-left: 5px;')
+    .concat(
+      if book.active_reservations.blank?
+        link_to('Criar nova reserva', new_reservation_path(params: { book_id: book.id }),
+                class: 'btn btn-primary')
+      end
+    )
   end
 
   def book_member_links(book)
     if book.active_reservations.blank?
-      link_to 'Reservar', new_reservation_path, class: 'btn btn-primary'
+      link_to 'Reservar', new_reservation_path(params: { book_id: book.id }),
+              class: 'btn btn-primary'
     else
       content_tag(:div, 'O livro não está disponível no momento', class: 'hint')
     end
