a0fcba4723d837f284feb253ebc5f6a6853318e8
leticiaoliveira5 <leoliveira994@gmail.com>
2022-10-14 10:30:10 -0300

    passa id do livro para url

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,14 +5,14 @@ module ApplicationHelper
   end
 
   def book_admin_links(book)
-    button_to 'Editar', edit_book_path, class: 'btn btn-primary'
+    button_to 'Editar', edit_book_path(book), class: 'btn btn-primary'
   end
 
   def book_member_links(book)
     if book.reservations.active.empty?
       button_to 'Reservar', new_reservation_path, class: 'btn btn-primary'
     else
-      content_tag(:div, 'O livro não está disponível no momento')
+      content_tag(:div, 'O livro não está disponível no momento', class: 'danger')
     end
   end
 end
