aab1f6f4dd26eef3552dddf851da8dfc767b7fa5
leticiaoliveira5 <leoliveira994@gmail.com>
2022-10-14 03:06:10 -0300

    melhora seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,7 +7,19 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 # Cria usuário admin
-User.create!(email: 'admin@library.net', password: '654321', role: 0, name: 'Admin')
+User.create!(email: 'admin@app.net', password: '123456', role: 0, name: 'Admin')
 
 # Cria usuário comum
 User.create!(email: 'maria@gmail.web', password: '654321', role: 1, name: 'Maria')
+
+# Cria categorias
+Category.create(name: 'Mistério')
+Category.create(name: 'Não Ficção')
+Category.create(name: 'Didático')
+Category.create(name: 'Poesia')
+
+# Cria autores
+Author.create(name: 'Clarice Lispector')
+Author.create(name: 'Stephen King')
+Author.create(name: 'Charles Dickinson')
+Author.create(name: 'Machado de Assis')
