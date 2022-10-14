8ab0c4dae715b472cc1b10fcae15b41cf23ea926
leticiaoliveira5 <leoliveira994@gmail.com>
2022-10-14 17:53:29 -0300

    remove warning rubocop

--- spec/features/admin_creates_user_spec.rb
+++ spec/features/admin_creates_user_spec.rb
@@ -11,16 +11,20 @@ RSpec.feature 'Admin creates user', type: :feature do
 
     expect(current_path).to eq new_user_path
 
-    fill_in :user_name, with: 'Nome'
-    fill_in :user_email, with: 'gustavo@bol.pt'
-    within('#user_role') { select 'member' }
-    fill_in :user_password, with: '123456'
-    fill_in :user_password_confirmation, with: '123456'
+    fill_in_user_attributes
     click_on :commit
 
     new_user = User.last
     expect(current_path).to eq user_path(new_user)
     expect(new_user.name).to eq 'Nome'
     expect(new_user.role).to eq 'member'
+  end
+
+  def fill_in_user_attributes
+    fill_in :user_name, with: 'Nome'
+    fill_in :user_email, with: 'gustavo@bol.pt'
+    within('#user_role') { select 'member' }
+    fill_in :user_password, with: '123456'
+    fill_in :user_password_confirmation, with: '123456'
   end
 end
